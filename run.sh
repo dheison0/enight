b20d353a50ad7e598b142a05f85c21f8223e6936
Dheison Gomes <dheisomgomes0@gmail.com>
2024-10-02 11:54:33 -0300

    Add: Migrations for the database Feat: run.sh now runs all migrations before build the application

--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
+#!/usr/bin/env bash
+
+echo "Loading config..."
+source .env
+source api/.env
+
+
+echo "Configuring database..."
+echo "Running migrations..."
+for f in $(find api/migrations -type f); do
+  echo "Running for $f..."
+  sqlite3 $DB_PATH "$(<$f)"
+done
+
+echo "Building web app..."
 cd web
 npm run build
-cd ../api
+cd ..
+
+echo "Building API server..."
+cd api
 go build
 cd ..
 ./api/api
