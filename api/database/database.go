6c224fe2a53dcc6552687a352027c0dcc044a9e8
Dheison Gomes <dheisomgomes0@gmail.com>
2024-10-04 16:42:38 -0300

    FIx: Enable foreign keys that wasn't working before

--- api/database/database.go
+++ api/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,6 +12,13 @@ var db *sql.DB
 func Init(path string) error {
 	var err error
 	db, err = sql.Open("sqlite3", path)
+	if err != nil {
+		return err
+	}
 	// db can be better configured!
+	_, err = db.Exec("PRAGMA foreign_keys = ON;")
+	if err != nil {
+		return errors.New("failed to enable foreign_keys! " + err.Error())
+	}
 	return err
 }
