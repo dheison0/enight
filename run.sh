47fdcce22f7e4afd727d8ab5ba2ba73d2e1953e0
Dheison Gomes <dheisomgomes0@gmail.com>
2024-12-01 18:34:59 -0300

    Feat: Avoid running web build every time run.sh runs when using dual-core CPU Fix: WEB_FILES_PATH was defined as WEB_FILES and wasn't working

--- run.sh
+++ run.sh
@@ -8,15 +8,17 @@ log() {
 }
 
 log "Building all system..."
-cd "$ROOT/web"
-npm run build &>/dev/null &
+if [ $(nproc) -gt 2 ]; then
+  cd "$ROOT/web"
+  npm run build &>/dev/null &
+fi
 cd "$ROOT/server"
-go build -ldflags="-extldflags=-w -s" || exit
+CGO_ENABLED=1 go build -ldflags="-extldflags=-w -s" || exit
 
 
 log "Running server..."
 mkdir "$ROOT/tmp"
-export WEB_FILES="$ROOT/web/dist"
+export WEB_FILES_PATH="$ROOT/web/dist"
 export DB_PATH="$ROOT/tmp/system.sqlite3"
 export BOT_DB_PATH="$ROOT/tmp/bot.sqlite3"
 export DEBUG=true
