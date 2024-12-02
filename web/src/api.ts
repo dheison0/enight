dd22556b8f7bb0b545ffc56ee6a8113b735085f9
Dheison Gomes <dheisomgomes0@gmail.com>
2024-12-02 18:23:51 -0300

    Fix: Autorization header was being called as Authentication & make isAuthenticated less verbose

--- web/src/api.ts
+++ web/src/api.ts
@@ -2,13 +2,13 @@ import { getAuthData, setAuthData } from "./storage"
 
 const API_BASE = "/api"
 
-export async function apiRequest(method: string, path: string, data?: object) {
+async function request(method: string, path: string, data?: object) {
   const auth = getAuthData()
   const headers: { [key: string]: string } = {
     "Content-Type": "application/json"
   }
   if (auth?.token) {
-    headers["Authentication"] = `Bearer ${auth.token}`
+    headers["Authorization"] = `Bearer ${auth.token}`
   }
   const body = data ? JSON.stringify(data) : null
   const response = await fetch(path, { method, headers, body })
@@ -16,7 +16,7 @@ export async function apiRequest(method: string, path: string, data?: object) {
 }
 
 export async function login(password: string): Promise<boolean> {
-  const response = await apiRequest("POST", `${API_BASE}/login`, { password })
+  const response = await request("POST", `${API_BASE}/login`, { password })
   if (response.status == 200) {
     const respData = await response.json()
     setAuthData(respData)
@@ -25,6 +25,7 @@ export async function login(password: string): Promise<boolean> {
 }
 
 export async function isAuthenticated(): Promise<boolean> {
-  const response = await apiRequest("GET", `${API_BASE}/ping`)
+  // TODO: Avoid making a request every time a verification is needed
+  const response = await request("GET", `/ping`)
   return response.status == 200
 }
